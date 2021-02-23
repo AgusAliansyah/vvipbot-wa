549aa34e85a99017ae594f32d28749a995cad583
rizky969 <79305089+rizky969@users.noreply.github.com>
2021-02-23 22:04:05 +0700

    index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' // Rizky Ganss
+            + 'ORG:VVIPINDO;\n' // BOT GANSS
+            + 'TEL;type=CELL;type=VOICE;waid=6289678654813:+62 89678654813\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
