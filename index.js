dcf343c8cc2e114529becfd27a1dfb8f4561c328
gityanno <81003881+gityanno@users.noreply.github.com>
2021-03-20 12:48:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'I'M BOT'; 
+const instagram = 'ardiyano_16- kaelMk_'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Ardi\n' // Nama kamu
+            + 'ORG:I'M;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6283899211400:+62 838-9921-1400\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
@@ -1450,7 +1450,7 @@ Silakan pilih :
 
 
 }
-//Hay gay
-//create @agusalnsyh don't delate this please
+//Hay guest
+//create @ardiyan don't delate this please
 	
 })
