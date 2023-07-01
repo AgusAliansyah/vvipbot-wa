1b9a464bf4cc63f97ddb4795f2b8f92639860aea
R2GANTENG <138259059+R2GANTENG@users.noreply.github.com>
2023-07-01 08:13:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:radenrizki\n' // Nama kamu
+            + 'ORG:R2VVIP;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6285703853047:+6285703853047\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
